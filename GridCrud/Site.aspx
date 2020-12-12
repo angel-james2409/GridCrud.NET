6ecaa735a88c9a922779a9c7baaddb64b157b84c
Angel <ngeljames2409@gmail.com>
2020-12-12 21:53:03 +0530

    final commit

---
+++ GridCrud/Site.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Site.aspx.cs" Inherits="GridCrud.Site" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="customerId" 
+  OnRowDataBound="OnRowDataBound" OnRowEditing="OnRowEditing" OnRowCancelingEdit="OnRowCancelingEdit"
+ OnRowUpdating="OnRowUpdating" OnRowDeleting="OnRowDeleting" EmptyDataText="No records has been added.">
+            <Columns>
+                <asp:TemplateField HeaderText="customerName" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblcustomerName" runat="server" Text='<%# Eval("customerName") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtcustomerName" runat="server" Text='<%# Eval("customerName") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+    <asp:TemplateField HeaderText="pass" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblpass" runat="server" Text='<%# Eval("pass") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtpass" runat="server" Text='<%# Eval("pass") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+                <asp:TemplateField HeaderText="walletAmt" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblwalletAmt" runat="server" Text='<%# Eval("walletAmt") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtwalletAmt" runat="server" Text='<%# Eval("walletAmt") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+                <asp:TemplateField HeaderText="phoneNo" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblphoneNo" runat="server" Text='<%# Eval("phoneNo") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtphoneNo" runat="server" Text='<%# Eval("phoneNo") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+                <asp:TemplateField HeaderText="addres" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lbladdres" runat="server" Text='<%# Eval("addres") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtaddres" runat="server" Text='<%# Eval("addres") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+
+    <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true" ItemStyle-Width="150"/>
+                
+            </Columns>
+        </asp:GridView>
+
+<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+<tr>
+    <td style="width: 150px">
+       customerName:<br />
+        <asp:TextBox ID="txtcustomerName" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        pass:<br />
+        <asp:TextBox ID="txtpass" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        walletAmt:<br />
+        <asp:TextBox ID="txtwalletAmt" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        phoneNo:<br />
+        <asp:TextBox ID="txtphoneNo" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        addres:<br />
+        <asp:TextBox ID="txtaddres" runat="server" Width="140" />
+    </td>
+    <td style="width: 100px">
+        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="Insert" />
+    </td>
+</tr>
+</table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ GridCrud/Site.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace GridCrud
+{
+    public partial class Site : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Customer_crud"))
+                {
+                    cmd.Parameters.AddWithValue("@Action", "SELECT");
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+
+            }
+        }
+        protected void Insert(object sender, EventArgs e)
+        {
+            string name = txtcustomerName.Text;
+            string pass = txtpass.Text;
+            string wallAmt = txtwalletAmt.Text;
+
+            string phono = txtphoneNo.Text;
+            string addres = txtaddres.Text;
+
+            string constr = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Customer_crud"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "INSERT");
+                    cmd.Parameters.AddWithValue("@customerName", name);
+                    cmd.Parameters.AddWithValue("@pass", pass);
+                    cmd.Parameters.AddWithValue("@walletAmt", wallAmt);
+                    cmd.Parameters.AddWithValue("@phoneNo", phono);
+                    cmd.Parameters.AddWithValue("@addres", addres);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+        }
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int customerId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string customerName = (row.FindControl("txtcustomerName") as TextBox).Text;
+            string pass = (row.FindControl("txtpass") as TextBox).Text;
+            int walletAmt = Convert.ToInt32((row.FindControl("txtwalletAmt") as TextBox).Text);
+            string phoneNo = (row.FindControl("txtphoneNo") as TextBox).Text;
+            string adress = (row.FindControl("txtaddres") as TextBox).Text;
+            string constr = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Customer_crud"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "UPDATE");
+                    cmd.Parameters.AddWithValue("@customerId", customerId);
+                    cmd.Parameters.AddWithValue("@customerName", customerName);
+                    cmd.Parameters.AddWithValue("@pass", pass);
+                    cmd.Parameters.AddWithValue("@walletAmt", walletAmt);
+                    cmd.Parameters.AddWithValue("@phoneNo", phoneNo);
+                    cmd.Parameters.AddWithValue("@addres", adress);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void OnRowCancelingEdit(object sender, EventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int customerId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Customer_crud"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "DELETE");
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+        }
+
+
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
+            {
+                (e.Row.Cells[5].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+            }
+        }
+    }
+}
